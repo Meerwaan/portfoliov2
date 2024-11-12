90cced7b277660efe45ae296bfa2881f550d29eb
Meerwaan <merwanlaouini@gmail.com>
2024-11-12 20:27:18 +0100

    add burger menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from './Views/Home/Home';
+import SideMenu from './Components/Home/SideMenu/SideMenu';
+import BurgerMenu from './Components/Home/BurgerMenu/BurgerMenu';
 
 function App() {
-  return (
-      <div className="App">
-        <Home />
-      </div>
-  );
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize(); // Appelle initiale pour définir l'état lors du chargement de la page
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <div className="App">
+            {isMobile ? <BurgerMenu /> : <SideMenu />}
+            <Home />
+        </div>
+    );
 }
 
 export default App;
